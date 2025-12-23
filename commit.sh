6fb6bac63a4a7b7ff7cdfa06f9bb0f25f4292875
lxltx2025 <lxltx2025@gmail.com>
2025-12-23 21:41:46 +0800

    添加批量提交脚本

---
+++ commit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git add readme.md
+git commit -m "项目说明文档"
+
+git add config.py
+git commit -m "项目配置"
+
+git add analyzer.py
+git commit -m "批量分析论文"
+
+git add get_paper.py
+git commit -m "批量下载文献"
